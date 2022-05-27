229e1a9737b00524fe11cf3c1ed1d8fa0907c00a
Daz Johnson <97320359+Dazjanae@users.noreply.github.com>
2022-05-27 12:08:19 -0500

    Add files via upload

---
+++ assets/js/main.js
@@ -0,0 +1,8 @@
+/* When the submit button is clicked a pop up will appear
+ user will need to click okay to remove the pop up */
+
+ document.querySelector('.submitButton').onclick = reservationConfirm
+
+ function reservationConfirm(){
+     alert('Reservation Confirmed')
+ }
