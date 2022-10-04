184f98d9c396dbde727d86a942ce49ad8995d986
Daz Johnson <97320359+Dazjanae@users.noreply.github.com>
2022-10-04 00:36:30 -0500

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,13 @@
 /* When the submit button is clicked a pop up will appear
  user will need to click okay to remove the pop up */
 
- document.querySelector('.submitButton').onclick = reservationConfirm
+ document.querySelector('.submitButton').addEventListener('click',reservationConfirm)
 
  function reservationConfirm(){
+    if (document.querySelector('.inputHere').value && document.querySelector('.inputHere1').value && document.querySelector('.inputHere2').value && document.querySelector('.inputHere3').value && document.querySelector('.inputHere4').value){
      alert('Reservation Confirmed')
+    }else {
+        alert('Please fill in the blank(s)')
+    }
  }
+
